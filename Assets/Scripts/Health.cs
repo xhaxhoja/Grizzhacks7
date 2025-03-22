fef7180e8fd5f4b1b9442a04ef12c51739d60b8c
ryanj <rjyounes@oakland.edu>
2025-03-22 19:35:17 -0400

    trying again

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public static float totalhealth = 1f;
+
+    //The purpose of this code is to to 
+}
